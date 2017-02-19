6b270c74ddf0284f4f5a991121f056917702fe28
lihaihao <mashy1024@gmail.com>
2017-02-19 22:11:41 +0800

    try to push1

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
-Carrierwave.configure do |config|
+CarrierWave.configure do |config|
   if Rails.env.production?
     config.fog_provider = 'fog'
     config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"]
+      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
 
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
 
